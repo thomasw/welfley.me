463763bc6670cf0d7d36642458a97df8b8edc4ba
Doug Beck <doug@douglasbeck.com>
2012-10-03 03:20:25 +0700

    never want to dynamically write JS with Django

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
 	init_project_slideshow();
+	
+	var GA_ACCOUNT = $("meta[name=ga-account]").attr("content");
+	alert(GA_ACCOUNT);
 	if(GA_ACCOUNT!=='nope') $.ga(GA_ACCOUNT);
+
 });
