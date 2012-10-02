df205033c7c4475782a8e5e0195996294c1696e6
Doug Beck <doug@douglasbeck.com>
2012-10-03 03:39:12 +0700

    cleaning up js

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,8 +1,5 @@
 $(document).ready(function(){
 	init_project_slideshow();
-	
 	var GA_ACCOUNT = $("meta[name=ga-account]").attr("content");
-	alert(GA_ACCOUNT);
 	if(GA_ACCOUNT!=='nope') $.ga(GA_ACCOUNT);
-
 });
