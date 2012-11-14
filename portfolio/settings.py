ba6d50b4a88e8d55c13e7e7420a3e80e0ca9cf88
Thomas W <thomas@matchstrike.net>
2012-11-13 21:51:41 -0800

    Make app 0 configuration for development
    local_settings.py is now optional. It can be used to override default, development settings.

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -1,4 +1,68 @@
+import logging
+import os
+
 from django import template
+
+
+SITE_NAME='Your Name'
+
+ADMINS = (
+    ('Your Name', 'info@site.tld'),
+)
+
+MANAGERS = ADMINS
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+# E-mail config
+EMAIL_SUBJECT_PREFIX = '[%s] ' % (SITE_NAME,)
+DEFAULT_FROM_EMAIL = 'info@site.tld'
+SEND_BROKEN_LINK_EMAILS = False
+
+if DEBUG:
+    EMAIL_PORT = 1025
+
+SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = os.path.join(SITE_ROOT, 'assets')
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = '/assets/'
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/media/'
+
+# Primary templates directory.
+TEMPLATE_DIRS = (os.path.join(SITE_ROOT, 'templates'),)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'site.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+# Logging Configuration
+logging.basicConfig(
+    level = logging.INFO,
+    format = '%(asctime)s %(levelname)s %(message)s',
+    filename = os.path.join(SITE_ROOT, 'site.log'),
+    filemode = 'a',
+)
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -67,12 +131,43 @@ INSTALLED_APPS = (
     'gunicorn',
 )
 
-# Login configuration
+
 LOGIN_URL = '/login/'
 LOGOUT_URL = '/logout/'
 LOGIN_REDIRECT_URL = '/'
 
-# djanog-compress settings.
+BROKER_HOST = "localhost"
+BROKER_PORT = 5672
+BROKER_USER = "hoot"
+BROKER_PASSWORD = "yay!"
+BROKER_VHOST = "hoot"
+CELERY_RESULT_BACKEND = "amqp"
+CELERY_IGNORE_RESULT = True
+CELERY_DISABLE_RATE_LIMITS = True
+CELERY_SEND_EVENTS = True
+CELERY_SEND_TASK_ERROR_EMAILS = True
+
+CELERYD_LOG_LEVEL = "INFO"
+CELERYD_LOG_FILE = 'celeryd.log'
+
+CELERYBEAT_SCHEDULE_FILENAME = "schedule.db"
+CELERYBEAT_LOG_FILE = 'celerybeat.log'
+CELERYBEAT_LOG_LEVEL = "INFO"
+
+# Maximum Messages Per Day. This is the maximum number of e-mail notices the
+# contact form will send you each day. Once the limit is reached, the contact
+# form will silently record new messages to the database, but will no longer
+# notify you of their creation.
+MAX_MESSAGES_PER_DAY = 10
+
+CACHE_BACKEND = 'memcached://127.0.0.1:11211/' if not DEBUG else 'dummy:///'
+CACHE_MIDDLEWARE_ANONYMOUS_ONLY = True
+CACHE_MIDDLEWARE_SECONDS = 60*60
+CACHE_MIDDLEWARE_KEY_PREFIX = 'welfleyme'
+
+COMPRESS = True
+COMPRESS_VERSION = True
+COMPRESS_AUTO = DEBUG
 COMPRESS_CSS = {
     'main': {
         'source_filenames': (
@@ -88,7 +183,6 @@ COMPRESS_CSS = {
         'output_filename': 'css/ie.r?.css'
     }
 }
-
 COMPRESS_JS = {
     'main': {
         'source_filenames': (
