5e6f6ed4413223fce69e024bfacbf0704e7b3af8
Thomas Welfley <thomas.welfley@gmail.com>
2019-09-19 11:17:47 -0700

    Install AOS

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init({});
 
 const root = document.getElementById('root');
 
