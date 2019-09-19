d25f25667e6eaf47f60801587185a1734a89e9d0
Thomas Welfley <thomas.welfley@gmail.com>
2019-09-19 11:23:39 -0700

    Home: Add placeholder content

--- src/views/Home.js
+++ src/views/Home.js
@@ -3,7 +3,71 @@ import React from 'react';
 import Header from '../theme/components/Header';
 
 function App() {
-  return <Header />;
+  return (
+    <React.Fragment>
+      <Header />
+      <section id="content">
+        <h1>Contact Me</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+          venenatis mollis feugiat. Proin vulputate tincidunt volutpat. Nam
+          vitae quam eget urna iaculis dignissim vel sit amet augue. In ultrices
+          in mauris ut blandit. Aenean et erat mi. Praesent vitae elit sodales,
+          tincidunt quam at, aliquam eros. Donec aliquam, nisl id congue
+          bibendum, ex lorem maximus leo, eu ultricies leo nibh quis eros.
+          Pellentesque in mauris erat. Suspendisse aliquam, libero et laoreet
+          pharetra, nisl arcu mattis tellus, ut auctor est felis quis quam.
+          Etiam ligula magna, congue at urna sit amet, aliquet accumsan lorem.
+          Nullam consectetur orci quis placerat fermentum. Phasellus consequat
+          nunc ut turpis interdum viverra. Morbi semper felis augue, quis
+          venenatis leo consectetur sed. Maecenas elementum sem ut justo
+          imperdiet convallis. Class aptent taciti sociosqu ad litora torquent
+          per conubia nostra, per inceptos himenaeos.
+        </p>
+        <p>
+          Phasellus sodales odio mi, sit amet blandit purus mattis ac. Morbi
+          feugiat turpis turpis, et mattis magna dictum id. Pellentesque ac
+          libero felis. Sed feugiat ante a turpis pulvinar, ut mattis tortor
+          cursus. Donec nec nisi lectus. Mauris id arcu mattis, sagittis mi in,
+          ullamcorper dui. Morbi vulputate efficitur lectus ut auctor. Proin et
+          nunc condimentum, cursus quam ut, sodales augue. Aliquam ante magna,
+          rhoncus at tortor et, imperdiet semper justo. Phasellus turpis augue,
+          tincidunt et ultricies eu, placerat non nisl.
+        </p>
+        <p>
+          Etiam nisi ex, aliquet vitae magna nec, elementum lacinia urna. Ut
+          tempus rutrum nisi eu tristique. Sed eget ex arcu. In id suscipit
+          lectus. Donec maximus, augue vitae maximus placerat, risus leo
+          facilisis odio, eu suscipit nisl leo non tortor. Integer dapibus
+          consectetur accumsan. Donec posuere porttitor ipsum quis euismod.
+          Morbi non laoreet nibh.
+        </p>
+        <p>
+          Nullam nec aliquam tellus. Duis molestie lacus justo, in vestibulum
+          tortor varius sit amet. Aliquam hendrerit dapibus bibendum. Nunc at
+          tortor id tellus gravida porta. Mauris vitae dui mi. Aliquam id ligula
+          id lacus ultricies pharetra eget id metus. Quisque sagittis lacus sit
+          amet elementum molestie. Nam at lorem laoreet, tempus dolor porta,
+          maximus sapien.
+        </p>
+        <p>
+          Nam vel lacus condimentum, tempus sapien eu, egestas nisi. Etiam nec
+          ullamcorper sapien. Vestibulum ante ipsum primis in faucibus orci
+          luctus et ultrices posuere cubilia Curae; Curabitur pellentesque
+          ligula urna, at viverra urna dignissim et. Proin fringilla bibendum
+          urna eu cursus. Quisque consectetur, neque porttitor feugiat varius,
+          ante augue ullamcorper nisi, non posuere risus velit ac felis. Ut
+          imperdiet fermentum velit et bibendum. Integer in lacinia quam. Etiam
+          mollis ex eget dictum ullamcorper. Integer felis lectus, pulvinar in
+          mauris in, congue faucibus sem. Fusce porttitor porta lorem nec porta.
+          Suspendisse dui justo, ultricies rhoncus nisl vestibulum, maximus
+          convallis massa. Integer fermentum enim metus, et blandit odio
+          tincidunt sit amet. Nam varius luctus mi, eu congue sem. Suspendisse
+          potenti.
+        </p>
+      </section>
+    </React.Fragment>
+  );
 }
 
 export default App;
