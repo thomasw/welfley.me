9fa63b03eb95dfed124ac7a2cf8bfccc2595bca6
Thomas Welfley <thomas.welfley@gmail.com>
2019-09-19 16:01:37 -0700

    Router: Prevent double route rendering

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from './views/Home';
 import FourOhFour from './views/FourOhFour';
@@ -9,8 +9,10 @@ import './App.css';
 function App() {
   return (
     <Router className="App">
-      <Route path="/" exact component={Home} />
-      <Route component={FourOhFour} />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route component={FourOhFour} />
+      </Switch>
     </Router>
   );
 }
