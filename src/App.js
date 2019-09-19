87c95d9346c316e9b348b3beee68941198cc3329
Thomas Welfley <thomas.welfley@gmail.com>
2019-09-19 12:13:54 -0700

    Install and configure routing via react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 // @flow
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Home from './views/Home';
+import FourOhFour from './views/FourOhFour';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router className="App">
+      <Route path="/" exact component={Home} />
+      <Route component={FourOhFour} />
+    </Router>
   );
 }
 
