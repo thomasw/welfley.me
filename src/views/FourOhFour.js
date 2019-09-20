de41623e88e10aad96b9b38299d1b4662e5b745d
Thomas Welfley <thomas.welfley@gmail.com>
2019-09-20 15:38:42 -0700

    Add meta information to pages

--- src/views/FourOhFour.js
+++ src/views/FourOhFour.js
@@ -1,10 +1,18 @@
 // @flow
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 export default function FourOhFour() {
   return (
-    <section id="content">
-      <h1>404: Not Found.</h1>
-    </section>
+    <React.Fragment>
+      <Helmet>
+        <title>404: Page not found</title>
+        <meta name="description" content="This page could not be found." />
+        <meta name="keywords" content="404, not found" />
+      </Helmet>
+      <section id="content">
+        <h1>404: Not Found</h1>
+      </section>
+    </React.Fragment>
   );
 }
