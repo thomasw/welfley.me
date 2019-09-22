0732eac33573f702f51656fc86e1a348e696d890
Thomas Welfley <thomas.welfley@gmail.com>
2019-09-21 18:08:53 -0700

    Add ContactSection component
    This component will contain a contact form and a prompt to use that form.

---
+++ src/contact/components/ContactSection.js
@@ -0,0 +1,41 @@
+// @flow
+import * as React from 'react';
+
+import Columns from '../../theme/components/Columns';
+
+import styles from './ContactSection.module.scss';
+
+export default function ContactSection() {
+  return (
+    <section id="contact-me" className={styles.container}>
+      <Columns>
+        <div>
+          <h1>Contact Me</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+            venenatis mollis feugiat. Proin vulputate tincidunt volutpat. Nam
+            vitae quam eget urna iaculis dignissim vel sit amet augue. In
+            ultrices in mauris ut blandit. Aenean et erat mi. Praesent vitae
+            elit sodales, tincidunt quam at, aliquam eros. Donec aliquam, nisl
+            id congue bibendum, ex lorem maximus leo, eu ultricies leo nibh quis
+            eros. Pellentesque in mauris erat. Suspendisse aliquam, libero et
+            laoreet pharetra, nisl arcu mattis tellus, ut auctor est felis quis
+            quam. Etiam ligula magna, congue at urna sit amet, aliquet accumsan
+            lorem. Nullam consectetur orci quis placerat fermentum. Phasellus
+            consequat nunc ut turpis interdum viverra. Morbi semper felis augue,
+            quis venenatis leo consectetur sed. Maecenas elementum sem ut justo
+            imperdiet convallis. Class aptent taciti sociosqu ad litora torquent
+            per conubia nostra, per inceptos himenaeos.
+          </p>
+        </div>
+        <div>
+          <p>This is a placeholder form.</p>
+          <input />
+          <input />
+          <textarea></textarea>
+          <button>Send</button>
+        </div>
+      </Columns>
+    </section>
+  );
+}
