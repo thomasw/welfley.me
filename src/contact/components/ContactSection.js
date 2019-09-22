fb8b98f0932ce146f93483ee0b7361dcaf40754a
Thomas Welfley <thomas.welfley@gmail.com>
2019-09-22 16:10:45 -0700

    ContactSection: add padding

--- src/contact/components/ContactSection.js
+++ src/contact/components/ContactSection.js
@@ -10,26 +10,14 @@ export default function ContactSection() {
   return (
     <section id="contact-me" className={styles.container}>
       <Columns>
-        <div>
+        <div className={styles.content}>
           <h1>Contact Me</h1>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-            venenatis mollis feugiat. Proin vulputate tincidunt volutpat. Nam
-            vitae quam eget urna iaculis dignissim vel sit amet augue. In
-            ultrices in mauris ut blandit. Aenean et erat mi. Praesent vitae
-            elit sodales, tincidunt quam at, aliquam eros. Donec aliquam, nisl
-            id congue bibendum, ex lorem maximus leo, eu ultricies leo nibh quis
-            eros. Pellentesque in mauris erat. Suspendisse aliquam, libero et
-            laoreet pharetra, nisl arcu mattis tellus, ut auctor est felis quis
-            quam. Etiam ligula magna, congue at urna sit amet, aliquet accumsan
-            lorem. Nullam consectetur orci quis placerat fermentum. Phasellus
-            consequat nunc ut turpis interdum viverra. Morbi semper felis augue,
-            quis venenatis leo consectetur sed. Maecenas elementum sem ut justo
-            imperdiet convallis. Class aptent taciti sociosqu ad litora torquent
-            per conubia nostra, per inceptos himenaeos.
+            venenatis mollis feugiat.
           </p>
         </div>
-        <ContactForm />
+        <ContactForm className={styles.form} />
       </Columns>
     </section>
   );
