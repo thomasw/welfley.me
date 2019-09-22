17fbae2279c33ee82002a5feebbd9bb57ab57587
Thomas Welfley <thomas.welfley@gmail.com>
2019-09-22 16:35:24 -0700

    Add explicit meta information to home route
    We were previously letting /'s meta data come from index.html. However, if you navigated to a page with alternate meta data and the navigated back to home, the meta data would not be restored to the original version. This fixes that problem at the cost of having to maintain the base meta information in two places (index.html and Home.js).

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,5 +1,7 @@
 // @flow
 import React from 'react';
+import { Helmet } from 'react-helmet';
+
 import Header from 'theme/components/Header';
 import SocialBar from 'theme/components/SocialBar';
 import ContactSection from 'contact/components/ContactSection';
@@ -8,6 +10,17 @@ import Footer from 'theme/components/Footer';
 export default function Home() {
   return (
     <React.Fragment>
+      <Helmet>
+        <title>Thomas Welfley: Portfolio</title>
+        <meta
+          name="description"
+          content="Thomas Welfley is a San Franciso based engineering manager that builds amazing teams and software."
+        />
+        <meta
+          name="keywords"
+          content="engineer, software engineer, python developer, developer, web developer, engineering manager, San Francisco, California, Welfley, Thomas, portfolio, resume"
+        />
+      </Helmet>
       <Header transitionAnchor="#contact-me">
         <h1>Thomas Welfley</h1>
         <p>
