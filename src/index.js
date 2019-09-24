a57421e4ebdd49d99c22277cc3630d574ef1597c
Thomas Welfley <thomas.welfley@gmail.com>
2019-09-24 10:16:43 -0700

    Render static pages and sitemap during builds

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 // @flow
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate, render } from 'react-dom';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 import App from 'App';
 import * as serviceWorker from 'serviceWorker';
 
-AOS.init({});
-
 const root = document.getElementById('root');
 
-root && ReactDOM.render(<App />, root);
+AOS.init();
+root && (root.hasChildNodes() ? hydrate(<App />, root) : render(<App />, root));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
