b63bd0e77c37ba7170fbec890af614d9213da8f7
Thomas Welfley <thomas.welfley@gmail.com>
2019-09-25 12:28:33 -0700

    NoticeManager: wire component to state

--- src/notices/components/NoticeManager.js
+++ src/notices/components/NoticeManager.js
@@ -1,21 +1,45 @@
 // @flow
 import React from 'react';
+import { connect } from 'react-redux';
 
+import type { State } from 'reducer';
 import { Overlay } from 'theme/components/layout';
+
+import { clearNotice } from '../actions';
+import { getNotice } from '../selectors';
+
 import styles from './NoticeManager.module.scss';
 
-type Props = {|
-  message?: string
+type MappedProps = {|
+  notice?: string
 |};
 
-export default function NoticeManager(props: Props) {
-  const { message } = props;
+type MappedActions = {|
+  clearNotice: () => Promise<null>
+|};
 
-  if (!message) return null;
+type Props = {|
+  ...MappedProps,
+  ...MappedActions
+|};
+
+function NoticeManager({ notice, clearNotice }: Props) {
+  if (!notice) return null;
 
   return (
-    <Overlay className={styles.container}>
-      <h1>{message}</h1>
+    <Overlay className={styles.container} onClick={clearNotice}>
+      <h1>{notice}</h1>
     </Overlay>
   );
 }
+
+function stateToProps(state: State): MappedProps {
+  return {
+    notice: getNotice(state.notices)
+  };
+}
+
+export default connect(
+  stateToProps,
+  { clearNotice }
+)(NoticeManager);
