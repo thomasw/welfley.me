7d75e0c78852a6e12fe368679db2f5c12d42b749
Thomas Welfley <thomas.welfley@gmail.com>
2019-09-26 14:41:07 -0700

    Button: add custom icon support

--- src/theme/components/button/Button.js
+++ src/theme/components/button/Button.js
@@ -2,15 +2,43 @@
 import * as React from 'react';
 import cx from 'classnames';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import styles from './Button.module.scss';
 
 type Props = {
-  className?: string
+  className?: string,
+  loading?: boolean,
+  iconLoadingClassName?: string,
+  icon: string,
+  children: React.Node
 };
 
 export default function Button(props: Props) {
-  const { className, ...inputProps } = props;
-  const combinedClass = cx(styles.button, className);
+  const {
+    className,
+    loading = false,
+    icon,
+    children,
+    iconLoadingClassName = '',
+    ...inputProps
+  } = props;
+  const combinedClass = cx(
+    styles.button,
+    { [styles.withIcon]: !!icon },
+    className
+  );
 
-  return <button className={combinedClass} {...inputProps} />;
+  const iconClass = cx({ [iconLoadingClassName]: loading });
+
+  return (
+    <button className={combinedClass} disabled={loading} {...inputProps}>
+      {icon && (
+        <span className={iconClass}>
+          <FontAwesomeIcon icon={icon} />
+        </span>
+      )}
+      {children}
+    </button>
+  );
 }
