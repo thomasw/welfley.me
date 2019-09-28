664df25a5d9bc4af16c1a87de2c14b4f14f7a26e
Thomas Welfley <thomas.welfley@gmail.com>
2019-09-28 16:29:42 -0700

    Add icon support for link buttons

--- src/theme/components/button/LinkButton.js
+++ src/theme/components/button/LinkButton.js
@@ -2,16 +2,32 @@
 import * as React from 'react';
 import cx from 'classnames';
 import { NavHashLink as Link } from 'react-router-hash-link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import styles from './Button.module.scss';
 
 type Props = {
-  className?: string
+  className?: string,
+  icon?: string,
+  children: React.Node
 };
 
 export default function LinkButton(props: Props) {
-  const { className, ...inputProps } = props;
-  const combinedClass = cx(styles.button, className);
+  const { className, icon, children, ...linkProps } = props;
+  const combinedClass = cx(
+    styles.button,
+    { [styles.withIcon]: !!icon },
+    className
+  );
 
-  return <Link className={combinedClass} {...inputProps} />;
+  return (
+    <Link className={combinedClass} {...linkProps}>
+      {icon && (
+        <span>
+          <FontAwesomeIcon icon={icon} />
+        </span>
+      )}
+      {children}
+    </Link>
+  );
 }
