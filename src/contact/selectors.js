65918ff8b12ecb298a26b53fbb92f94bc7b857b1
Thomas Welfley <thomas.welfley@gmail.com>
2019-09-29 16:59:31 -0700

    ContactForm: add didSendingMessageFail selector

--- src/contact/selectors.js
+++ src/contact/selectors.js
@@ -1,7 +1,11 @@
 // @flow
-import { isLoading } from 'redux-thunk-monitor';
+import { isLoading, hasError } from 'redux-thunk-monitor';
 import type { State } from 'reducer';
 
 export function isSendingMessage(state: State) {
   return isLoading(state, 'sendMessage');
+}
+
+export function didSendingMessageFail(state: State) {
+  return hasError(state, 'sendMessage');
 }
