0e812c57d3c31634625c16323f8469cf571330b4
Thomas Welfley <thomas.welfley@gmail.com>
2019-09-29 16:59:08 -0700

    Make NoticeManager transition reusable

--- src/notices/components/NoticeManager.js
+++ src/notices/components/NoticeManager.js
@@ -1,8 +1,8 @@
 // @flow
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
+import FadeTransition from 'theme/components/FadeTransition';
 import { Overlay } from 'theme/components/layout';
 
 import { clearNotice } from '../actions';
@@ -11,22 +11,19 @@ import { getNotice } from '../selectors';
 import styles from './NoticeManager.module.scss';
 
 export default function NoticeManager() {
-  const { container, placeholder, ...transitionStyles } = styles;
   const notice = useSelector(getNotice);
   const dispatch = useDispatch();
   const boundClearNotice = () => dispatch(clearNotice());
 
   return (
-    <TransitionGroup>
-      <CSSTransition key={notice} classNames={transitionStyles} timeout={500}>
-        {notice ? (
-          <Overlay className={container} onClick={boundClearNotice}>
-            <h1>{notice}</h1>
-          </Overlay>
-        ) : (
-          <div className={placeholder} />
-        )}
-      </CSSTransition>
-    </TransitionGroup>
+    <FadeTransition sceneId={notice}>
+      {notice ? (
+        <Overlay className={styles.container} onClick={boundClearNotice}>
+          <h1>{notice}</h1>
+        </Overlay>
+      ) : (
+        <div className={styles.placeholder} />
+      )}
+    </FadeTransition>
   );
 }
