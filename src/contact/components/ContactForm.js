2e3d1020edd5153fb7940c26420f29c25d1e1f0e
Thomas Welfley <thomas.welfley@gmail.com>
2019-09-29 17:00:44 -0700

    ContactForm: ignore sending errors
    Errors will be saved to the state and handled elsewhere.

--- src/contact/components/ContactForm.js
+++ src/contact/components/ContactForm.js
@@ -27,7 +27,10 @@ export default function ContactForm() {
 
   const handleSubmit = event => {
     event.preventDefault();
-    actions.sendMessage(formData).then(resetForm);
+    actions
+      .sendMessage(formData)
+      .then(resetForm)
+      .catch(() => {});
   };
 
   const updateField = e => {
