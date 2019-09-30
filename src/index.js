4272f42ec677a2bc0a301c9bf9d0401c5040f12b
Thomas Welfley <thomas.welfley@gmail.com>
2019-09-30 10:05:51 -0700

    Remove service worker implementation

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 import App from 'App';
-import * as serviceWorker from 'serviceWorker';
 
 const root = document.getElementById('root');
 
@@ -14,8 +13,3 @@ smoothscroll.polyfill();
 AOS.init();
 
 root && (root.hasChildNodes() ? hydrate(<App />, root) : render(<App />, root));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
