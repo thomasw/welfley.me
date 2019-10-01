6c7c3bdeeef87c4f59a636048714fe08b1c123ed
Thomas Welfley <thomas.welfley@gmail.com>
2019-10-01 12:52:49 -0700

    ScrollManager: don't accept children
    This component just applies a side effect. There's no reason to wrap the router contents.

--- src/router/components/ScrollManager.js
+++ src/router/components/ScrollManager.js
@@ -1,18 +1,17 @@
 // @flow
-import * as React from 'react';
+import React from 'react';
 import { withRouter } from 'react-router';
 
 type Props = {
-  children: React.Node,
   location: { pathname: string }
 };
 
-function ScrollManager({ children, location: { pathname } }: Props) {
+function ScrollManager({ location: { pathname } }: Props) {
   React.useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
 
-  return children || null;
+  return null;
 }
 
 export default withRouter(ScrollManager);
