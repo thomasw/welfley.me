dcb8089837d8c6b2d64603fbda94442738b1121d
Thomas Welfley <thomas.welfley@gmail.com>
2019-10-01 13:57:28 -0700

    ScrollManager: ignore initial pathname change
    Not ignoring the initial app's hydrated pathname means that we will sometimes scroll to the top rather than honoring whatever hash was specified by the incoming request. This was particularly evidence in Firefox.

--- src/router/components/ScrollManager.js
+++ src/router/components/ScrollManager.js
@@ -1,13 +1,23 @@
 // @flow
-import React from 'react';
+import { useEffect } from 'react';
 import { withRouter } from 'react-router';
 
 type Props = {
   location: { pathname: string }
 };
 
+let MOUNTED = false;
+
 function ScrollManager({ location: { pathname } }: Props) {
-  React.useEffect(() => {
+  useEffect(() => {
+    // Disregard the first pathname change. That's the initial url and
+    // we want to honor whatever #hash is specified by not scrolling up to the
+    // top.
+    if (!MOUNTED) {
+      MOUNTED = true;
+      return;
+    }
+
     window.scrollTo(0, 0);
   }, [pathname]);
 
