247f498b4d21693afd547c9870fb91774f344278
Thomas Welfley <thomas.welfley@gmail.com>
2019-10-02 12:20:47 -0700

    Install and configure sentry

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // @flow
 import React from 'react';
 import { hydrate, render } from 'react-dom';
+import * as Sentry from '@sentry/browser';
 import smoothscroll from 'smoothscroll-polyfill';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -9,6 +10,11 @@ import App from 'App';
 import { hasWebPSupport } from './featureDetection';
 
 const root = document.getElementById('root');
+
+process.env.NODE_ENV === 'production' &&
+  Sentry.init({
+    dsn: 'https://cfe35a9e688e4f4b83d3c1c5dcd9cc3b@sentry.io/1768567'
+  });
 
 !navigator.webdriver &&
   hasWebPSupport('lossless', (feature, result) => {
