09158e75ea285f7723a993c18cdf94b26201a906
Thomas Welfley <thomas.welfley@gmail.com>
2019-10-05 15:35:51 -0700

    stylelint: correct invalid glob

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")/.."
 
 status=0
 
-npx stylelint src/**/*.{css,scss} || status=1
+npx stylelint "src/**/*.{css,scss}" || status=1
 npx flow || status=1
 npx eslint src/ || status=1
 
