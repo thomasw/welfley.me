65fbd0508a3538a7db88d828f970756a2fa04b08
Thomas Welfley <thomas.welfley@gmail.com>
2019-11-03 18:23:11 -0800

    YolaDemo: Add position title

--- src/portfolio/components/yolademo/YolaDemo.js
+++ src/portfolio/components/yolademo/YolaDemo.js
@@ -8,6 +8,7 @@ export default function YolaDemo() {
     <div className={styles.container}>
       <div className={styles.content}>
         <h1>Yola</h1>
+        <h2>Vice President of Engineering</h2>
         <h2>
           13 million users. 9 million websites.
           <span className={styles.cursor}>&nbsp;</span>
